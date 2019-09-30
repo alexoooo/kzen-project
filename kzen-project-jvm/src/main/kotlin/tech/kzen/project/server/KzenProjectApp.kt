f7af06b0f9e506bb79c327494252930d2aca7957
alexoooo <ostrovsky.alex@gmail.com>
2019-09-30 07:49:08 -0400

    release 0.1.0, updated dependencies, added kzenAutoInit invocation

--- kzen-project-jvm/src/main/kotlin/tech/kzen/project/server/KzenProjectApp.kt
+++ kzen-project-jvm/src/main/kotlin/tech/kzen/project/server/KzenProjectApp.kt
@@ -4,15 +4,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.reactive.config.EnableWebFlux
+import tech.kzen.auto.server.kzenAutoInit
 
 
 @EnableWebFlux
 @SpringBootApplication
 @ComponentScan(basePackages = ["tech.kzen.auto.server"])
-//@ComponentScan(basePackageClasses = [tech.kzen.auto.server.KzenAutoApp.class])
 class KzenProjectApp
 
 
 fun main(args: Array<String>) {
+    kzenAutoInit()
     runApplication<KzenProjectApp>(*args)
 }
