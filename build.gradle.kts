7da8e1f63d028c38196fe60a24b4c3aaeb8f459b
alexoooo <alex@ostrovsky.biz>
2020-06-29 09:46:49 -0400

    release 0.15.1, dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "tech.kzen.project"
-    version = "0.14.1"
+    version = "0.15.0"
 
     repositories {
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,16 +1,17 @@
 //const val kotlinVersion = "1.4-M1"
 const val kotlinVersion = "1.3.72"
 
-const val springBootVersion = "2.2.6.RELEASE"
+//const val springBootVersion = "2.2.6.RELEASE"
+const val springBootVersion = "2.3.1.RELEASE"
 const val dependencyManagementVersion = "1.0.9.RELEASE"
 
-const val coroutinesVersion = "1.3.5"
+const val coroutinesVersion = "1.3.7"
 const val serializationVersion = "0.20.0"
 
 const val wrapperKotlinVersion = "pre.103-kotlin-1.3.72"
 
-const val kzenLibVersion = "0.14.2"
-const val kzenAutoVersion = "0.14.1"
+const val kzenLibVersion = "0.15.0"
+const val kzenAutoVersion = "0.15.0"
 
 const val immutaleJsVersion = "3.8.2"
 const val dexxVersion = "0.7"
@@ -30,9 +31,9 @@ const val guavaVersion = "29.0-jre"
 const val seleniumVersion = "3.141.59"
 const val commonsCompressVersion = "1.20"
 
-const val materialUiCoreVersion = "4.9.14"
+const val materialUiCoreVersion = "4.10.2"
 const val materialUiIconsVersion = "4.9.1"
-const val cropperJsVersion = "1.5.6"
+const val cropperJsVersion = "1.5.7"
 const val lodashVersion = "4.17.15"
 const val reactSelectVersion = "3.1.0"
 
