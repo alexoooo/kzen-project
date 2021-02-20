0805e58e5ef154f8a098bd32764efe71ce96f4ac
alexoooo <alex@ostrovsky.biz>
2021-02-19 19:34:39 -0500

    release 0.21.0, dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "tech.kzen.project"
-    version = "0.20.0"
+    version = "0.21.0"
 
     repositories {
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
--- kzen-project-jvm/build.gradle.kts
+++ kzen-project-jvm/build.gradle.kts
@@ -59,6 +59,7 @@ tasks.withType<ProcessResources> {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
+        useIR = true
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "15"
     }
