b79bf2925f1edd649b03f91f87ce10ec4ce9dd76
alexoooo <alex@ostrovsky.biz>
2021-11-20 11:55:16 -0500

    release 0.24.0, dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "tech.kzen.project"
-    version = "0.24.0"
+    version = "0.24.1"
 
     repositories {
         mavenCentral()
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
-const val kotlinVersion = "1.5.31"
+const val kotlinVersion = "1.6.0"
 const val jvmTargetVersion = "16"
-const val jvmToolchainVersion = 16
-const val shadowVersion = "7.0.0"
-const val springBootVersion = "2.5.5"
+const val jvmToolchainVersion = 17
+const val shadowVersion = "7.1.0"
+const val springBootVersion = "2.6.0"
 const val dependencyManagementVersion = "1.0.11.RELEASE"
 
-const val kzenAutoVersion = "0.24.0"
+const val kzenAutoVersion = "0.24.1"
