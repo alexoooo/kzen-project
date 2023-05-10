b6ccde9f95d37fd5663f34ad7a21ee7a2d0102bf
alexoooo <alex@ostrovsky.biz>
2023-05-09 21:33:35 -0400

    version bump

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,4 +2,4 @@ const val kotlinVersion = "1.8.20"
 const val jvmTargetVersion = "19"
 const val javaVersion = 19
 const val jvmToolchainVersion = 19
-const val kzenAutoVersion = "0.26.0"
+const val kzenAutoVersion = "0.27.0-SNAPSHOT"
