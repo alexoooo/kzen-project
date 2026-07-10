4cf898c524b1be2811b58ceea9e17703c23a917c
alexoooo <alex@ostrovsky.biz>
2026-07-10 11:12:35 -0400

    release 0.29.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ const val jvmTargetVersion = "26"
 const val javaVersion = 26
 const val jvmToolchainVersion = 26
 
-const val kzenLibVersion = "0.29.1-SNAPSHOT"
-const val kzenAutoVersion = "0.29.1-SNAPSHOT"
+const val kzenLibVersion = "0.29.1"
+const val kzenAutoVersion = "0.29.1"
 
 const val esbuildVersion = "0.28.1"
