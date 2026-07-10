f50a02747868b44f83c271c8854db721cb3ddc78
alexoooo <alex@ostrovsky.biz>
2026-07-10 17:31:57 -0400

    version bump, release/distribution improvements

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ const val jvmTargetVersion = "26"
 const val javaVersion = 26
 const val jvmToolchainVersion = 26
 
-const val kzenLibVersion = "0.29.2-SNAPSHOT"
-const val kzenAutoVersion = "0.29.2-SNAPSHOT"
+const val kzenLibVersion = "0.30.0-SNAPSHOT"
+const val kzenAutoVersion = "0.30.0-SNAPSHOT"
 
 const val esbuildVersion = "0.28.1"
